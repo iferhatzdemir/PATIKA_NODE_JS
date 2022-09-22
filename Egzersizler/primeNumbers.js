b1cdc84ba000e00b8b46490dc81e97ee973277be
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-09-22 22:37:01 +0300

    İki Sayı arasındaki Asal sayıları console a yazdıran program yazıldı burada node dosya adı parametre  olarak çalıştırıp sonucu görfük

---
+++ Egzersizler/primeNumbers.js
@@ -0,0 +1,33 @@
+const inputArray = process.argv.slice(2);
+let showPrimeNumbers = (lowNumber, highNumber) => {
+  if (lowNumber > highNumber) {
+    for (let i = highNumber; i <= lowNumber; i++) {
+      let isPrime = true;
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        console.log(i);
+      }
+    }
+  } else {
+    for (let i = lowNumber; i <= highNumber; i++) {
+      let isPrime = true;
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        console.log(i);
+      }
+    }
+  }
+};
+
+showPrimeNumbers(inputArray[0] * 1, inputArray[1] * 1); //1 ile çarptığımız da number tipine çeviriyoruz.
+console.log(process.argv.slice(2));
